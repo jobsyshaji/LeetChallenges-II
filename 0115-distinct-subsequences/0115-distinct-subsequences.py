96797c9e625087d41bac3e1c5ef4cbb863f3fd1e
Jobsy Shaji <jobsyshaji28@gmail.com>
2026-06-25 12:57:08 +0530

    Time: 1977 ms (5.04%), Space: 108.4 MB (10.68%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numDistinct(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+
+        memo = {}
+
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+
+            if i == len(s):
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if s[i] == t[j]:
+                memo[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+            else:
+                memo[(i, j)] = dfs(i + 1, j)
+
+            return memo[(i, j)]
+
+        return dfs(0, 0)
